a580b715dd64e776959ffa985894b76598fa85df
Kavya <jkavya3@gmail.com>
2025-05-11 20:13:20 +0530

    May 11 2025

---
+++ src/JavaBasics/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        for(int i=1;i<=10;i++)
+        {
+            System.out.printf(5 + "*" + i + "=" + (5*i) +"\n");
+        }
+    }
+}
